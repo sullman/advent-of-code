c027aad6157c9cc81df5d1e3cd490a73affef37b
Schuyler Ullman <schuyler.ullman@gmail.com>
2022-12-05 18:03:40 -0800

    Day 5, part 2

--- 2022/05/day05.go
+++ 2022/05/day05.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"bufio"
-	"container/list"
 	"fmt"
 	"os"
 )
 
+func reverse(s []byte) []byte {
+	for i, j := 0, len(s) - 1; i < j; i, j = i + 1, j - 1 {
+		s[i], s[j] = s[j], s[i]
+	}
+
+	return s
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	var allStacks []*list.List
+	var allStacks [][]byte
 	initialized := false
 	numStacks := 0
+	part2 := true
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -20,26 +28,32 @@ func main() {
 			var numToMove, from, to int
 			n, _ := fmt.Sscanf(line, "move %d from %d to %d", &numToMove, &from, &to)
 			if n == 3 {
-				for ; numToMove > 0; numToMove-- {
-					elem := allStacks[from - 1].Back()
-					allStacks[to - 1].PushBack(elem.Value)
-					allStacks[from - 1].Remove(elem)
+				from--
+				to--
+				if part2 {
+					allStacks[to] = append(allStacks[to], allStacks[from][len(allStacks[from]) - numToMove:]...)
+				} else {
+					allStacks[to] = append(allStacks[to], reverse(allStacks[from][len(allStacks[from]) - numToMove:])...)
 				}
+				allStacks[from] = allStacks[from][:len(allStacks[from]) - numToMove]
 			}
 		} else if line[1] == '1' {
 			initialized = true
+			for i := 0; i < numStacks; i++ {
+				reverse(allStacks[i])
+			}
 		} else {
 			if numStacks == 0 {
 				numStacks = (len(line) + 1) / 4
-				allStacks = make([]*list.List, numStacks)
+				allStacks = make([][]byte, numStacks)
 				for i := 0; i < numStacks; i++ {
-					allStacks[i] = list.New()
+					allStacks[i] = make([]byte, 0, 25)
 				}
 			}
 
 			for i := 0; i < numStacks; i++ {
 				if line[4 * i + 1] != ' ' {
-					allStacks[i].PushFront(line[4 * i + 1])
+					allStacks[i] = append(allStacks[i], line[4 * i + 1])
 				}
 			}
 		}
@@ -48,7 +62,7 @@ func main() {
 	result := ""
 
 	for i := 0; i < numStacks; i++ {
-		result = result + string(allStacks[i].Back().Value.(byte))
+		result = result + string(allStacks[i][len(allStacks[i]) - 1])
 	}
 
 	fmt.Println(result)
