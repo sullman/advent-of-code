85c3e7c14912dbfe84d4a4681ff86fab802ec909
Schuyler Ullman <schuyler.ullman@gmail.com>
2022-12-06 06:53:02 -0800

    Day 6, screw around with rolling counts instead of nested loops

--- 2022/06/day06.go
+++ 2022/06/day06.go
@@ -8,29 +8,28 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	bytesRead := 0
-	numUnique := 14
+	var rollingCounts [26]int
+	windowSize := 4
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		for i := 0; i < len(line) - numUnique; i++ {
-			dup := false
-			for j := i; j < i + numUnique - 1; j++ {
-				for k := j + 1; k < i + numUnique; k++ {
-					if line[j] == line[k] {
+		for i := 0; i < len(line); i++ {
+			rollingCounts[line[i] - 'a']++
+			if i >= windowSize {
+				rollingCounts[line[i - windowSize] - 'a']--
+				dup := rollingCounts[line[i - windowSize] - 'a'] != 1 || rollingCounts[line[i] - 'a'] != 1
+				for j := 0; !dup && j < len(rollingCounts); j++ {
+					if rollingCounts[j] > 1 {
 						dup = true
-						break
 					}
 				}
-			}
 
-			if !dup {
-				bytesRead = i + numUnique
-				break
+				if !dup {
+					fmt.Println(i + 1)
+					break
+				}
 			}
 		}
-
-		fmt.Println(bytesRead)
 	}
 }
