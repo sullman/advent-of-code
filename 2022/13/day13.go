d8b719fbce5b7a9e5305b7cec741c2c0e3553358
Schuyler Ullman <schuyler.ullman@gmail.com>
2022-12-13 10:01:33 -0800

    Day 13, part 2

--- 2022/13/day13.go
+++ 2022/13/day13.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 const (
@@ -12,91 +13,125 @@ const (
 	CloseList = -1
 )
 
-type Tokenizer struct {
+type Packet struct {
 	str string
 	index int
 	bufferedVal int
 	bufferedParens int
+	special bool
 }
 
-func NewTokenizer(str string) *Tokenizer {
-	return &Tokenizer{str: str, bufferedVal: -1}
+func NewPacket(str string) *Packet {
+	p := &Packet{str: str}
+	p.Reset()
+	return p
 }
 
-func (t *Tokenizer) Next() int {
-	if t.bufferedVal >= 0 {
-		val := t.bufferedVal
-		t.bufferedVal = -1
+func (p *Packet) Reset() {
+	p.index = 0
+	p.bufferedVal = -1
+	p.bufferedParens = 0
+}
+
+func (p *Packet) Next() int {
+	if p.bufferedVal >= 0 {
+		val := p.bufferedVal
+		p.bufferedVal = -1
 		return val
-	} else if t.bufferedParens > 0 {
-		t.bufferedParens--
+	} else if p.bufferedParens > 0 {
+		p.bufferedParens--
 		return CloseList
-	} else if t.index >= len(t.str) {
+	} else if p.index >= len(p.str) {
 		return EOL
 	}
 
-	if t.str[t.index] == '[' {
-		t.index++
+	if p.str[p.index] == '[' {
+		p.index++
 		return OpenList
-	} else if t.str[t.index] == ']' {
-		t.index++
+	} else if p.str[p.index] == ']' {
+		p.index++
 		return CloseList
 	}
 
 	val := 0
-	for ; t.str[t.index] >= '0' && t.str[t.index] <= '9'; t.index++ {
-		val = 10 * val + int(t.str[t.index] - '0')
+	for ; p.str[p.index] >= '0' && p.str[p.index] <= '9'; p.index++ {
+		val = 10 * val + int(p.str[p.index] - '0')
 	}
 
-	if t.str[t.index] == ',' {
-		t.index++
+	if p.str[p.index] == ',' {
+		p.index++
 	}
 
 	return val
 }
 
-func (t *Tokenizer) SynthesizeList(val int) {
-	t.bufferedVal = val
-	t.bufferedParens++
+func (p *Packet) SynthesizeList(val int) {
+	p.bufferedVal = val
+	p.bufferedParens++
+}
+
+func Compare(left *Packet, right *Packet) bool {
+	left.Reset()
+	right.Reset()
+
+	for l, r := left.Next(), right.Next(); l != EOL && r != EOL; l, r = left.Next(), right.Next() {
+		if l == r {
+			// Doesn't matter what they are if they're the same, just keep going
+			continue
+		} else if r == CloseList {
+			return false
+		} else if l == CloseList {
+			return true
+		} else if l == OpenList {
+			right.SynthesizeList(r)
+		} else if r == OpenList {
+			left.SynthesizeList(l)
+		} else if l < r {
+			return true
+		} else { // l > r
+			return false
+		}
+	}
+
+	panic("Packets were the same?")
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	index := 0
 	sum := 0
+	packets := make([]*Packet, 2)
+	packets[0] = NewPacket("[[2]]")
+	packets[0].special = true
+	packets[1] = NewPacket("[[6]]")
+	packets[1].special = true
 
 	for scanner.Scan() {
 		index++
-		left := NewTokenizer(scanner.Text())
+		left := NewPacket(scanner.Text())
+		packets = append(packets, left)
 		scanner.Scan()
-		right := NewTokenizer(scanner.Text())
+		right := NewPacket(scanner.Text())
+		packets = append(packets, right)
 		scanner.Scan()
 
-		for l, r := left.Next(), right.Next(); l != EOL && r != EOL; l, r = left.Next(), right.Next() {
-			if l == r {
-				// Doesn't matter what they are if they're the same, just keep going
-				continue
-			} else if r == CloseList {
-				fmt.Printf("Pair %d is in the wrong order, right ran out of items\n", index)
-				break
-			} else if l == CloseList {
-				fmt.Printf("Pair %d is in the right order, left ran out of items\n", index)
-				sum += index
-				break
-			} else if l == OpenList {
-				right.SynthesizeList(r)
-			} else if r == OpenList {
-				left.SynthesizeList(l)
-			} else if l < r {
-				fmt.Printf("Pair %d is in the right order, %d < %d\n", index, l, r)
-				sum += index
-				break
-			} else { // l > r
-				fmt.Printf("Pair %d is in the wrong order, %d > %d\n", index, l, r)
-				break
-			}
+		if Compare(left, right) {
+			sum += index
 		}
 	}
 
 	fmt.Println(sum)
+
+	sort.Slice(packets, func(i, j int) bool {
+		return Compare(packets[i], packets[j])
+	})
+
+	product := 1
+	for i, p := range packets {
+		if p.special {
+			product *= i + 1
+		}
+	}
+
+	fmt.Println(product)
 }
