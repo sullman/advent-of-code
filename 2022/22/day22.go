c32eb6b10e7d07a53ad5cb5b6a526af89761e35e
Schuyler Ullman <schuyler.ullman@gmail.com>
2022-12-22 14:57:05 -0800

    Day 22, part 2 (with hardcoded transitions :/ )

--- 2022/22/day22.go
+++ 2022/22/day22.go
@@ -19,6 +19,13 @@ var Directions = []Movement {
 }
 
 const (
+	Right = iota
+	Down
+	Left
+	Up
+)
+
+const (
 	Open = '.'
 	Void = ' '
 	Wall = '#'
@@ -33,6 +40,80 @@ func GetSpaceType(grid *[]string, row int, col int) byte {
 	}
 
 	return (*grid)[row][col]
+}
+
+func WrapPart1(grid *[]string, row *int, col *int, facing *int) bool {
+	wrappedRow, wrappedCol := *row, *col
+	reverse := Directions[(*facing + 2) % 4]
+	for ; GetSpaceType(grid, wrappedRow, wrappedCol) != Void; wrappedRow, wrappedCol = wrappedRow + reverse.row, wrappedCol + reverse.col {}
+	wrappedRow += Directions[*facing].row
+	wrappedCol += Directions[*facing].col
+	if GetSpaceType(grid, wrappedRow, wrappedCol) == Open {
+		*row, *col = wrappedRow, wrappedCol
+	} else {
+		return false
+	}
+
+	return true
+}
+
+func WrapPart2(grid *[]string, row *int, col *int, facing *int) bool {
+	// Hardcode the transitions :[
+	newRow, newCol := *row, *col
+	newFacing := *facing
+	if *facing == Left && *row < 50 {
+		newFacing = Right
+		newRow, newCol = 149 - *row, 0
+	} else if *facing == Left && *row < 100 {
+		newFacing = Down
+		newRow, newCol = 100, *row - 50
+	} else if *facing == Left && *row < 150 {
+		newFacing = Right
+		newRow, newCol = 149 - *row, 50
+	} else if *facing == Left && *row < 200 {
+		newFacing = Down
+		newRow, newCol = 0, *row - 100
+	} else if *facing == Up && *col < 50 {
+		newFacing = Right
+		newRow, newCol = *col + 50, 50
+	} else if *facing == Up && *col < 100 {
+		newFacing = Right
+		newRow, newCol = *col + 100, 0
+	} else if *facing == Up && *col < 150 {
+		newFacing = Up
+		newRow, newCol = 199, *col - 100
+	} else if *facing == Right && *row < 50 {
+		newFacing = Left
+		newRow, newCol = 149 - *row, 99
+	} else if *facing == Right && *row < 100 {
+		newFacing = Up
+		newRow, newCol = 49, 50 + *row
+	} else if *facing == Right && *row < 150 {
+		newFacing = Left
+		newRow, newCol = 149 - *row, 149
+	} else if *facing == Right && *row < 200 {
+		newFacing = Up
+		newRow, newCol = 149, *row - 100
+	} else if *facing == Down && *col < 50 {
+		newFacing = Down
+		newRow, newCol = 0, 100 + *col
+	} else if *facing == Down && *col < 100 {
+		newFacing = Left
+		newRow, newCol = *col + 100, 49
+	} else if *facing == Down && *col < 150 {
+		newFacing = Left
+		newRow, newCol = *col - 50, 99
+	} else {
+		panic("Huh?")
+	}
+
+	if (*grid)[newRow][newCol] == Wall {
+		return false
+	}
+
+	*facing = newFacing
+	*row, *col = newRow, newCol
+	return true
 }
 
 func main() {
@@ -52,56 +133,53 @@ func main() {
 	scanner.Scan()
 	instructions := scanner.Text()
 
-	row := 0
-	col := 0
-	facing := 0
+	wrapFunctions := []func(*[]string, *int, *int, *int) bool{WrapPart1, WrapPart2}
 
-	for ; grid[row][col] != Open; col++ {}
+	for _, wrap := range wrapFunctions {
+		row := 0
+		col := 0
+		facing := 0
 
-	for i := 0; i < len(instructions); i++ {
-		numSpaces := 0
+		for ; grid[row][col] != Open; col++ {}
 
-		for ; i < len(instructions) && instructions[i] <= '9'; i++ {
-			numSpaces = numSpaces * 10 + int(instructions[i] - '0')
-		}
+		for i := 0; i < len(instructions); i++ {
+			numSpaces := 0
 
-		for n := 0; n < numSpaces; n++ {
-			newRow := row + Directions[facing].row
-			newCol := col + Directions[facing].col
-			switch GetSpaceType(&grid, newRow, newCol) {
-			case Open:
-				row, col = newRow, newCol
-			case Wall:
-				break
-			case Void:
-				wrappedRow, wrappedCol := row, col
-				reverse := Directions[(facing + 2) % 4]
-				for ; GetSpaceType(&grid, wrappedRow, wrappedCol) != Void; wrappedRow, wrappedCol = wrappedRow + reverse.row, wrappedCol + reverse.col {}
-				wrappedRow += Directions[facing].row
-				wrappedCol += Directions[facing].col
-				if GetSpaceType(&grid, wrappedRow, wrappedCol) == Open {
-					row, col = wrappedRow, wrappedCol
-				} else {
+			for ; i < len(instructions) && instructions[i] <= '9'; i++ {
+				numSpaces = numSpaces * 10 + int(instructions[i] - '0')
+			}
+
+			for n := 0; n < numSpaces; n++ {
+				newRow := row + Directions[facing].row
+				newCol := col + Directions[facing].col
+				switch GetSpaceType(&grid, newRow, newCol) {
+				case Open:
+					row, col = newRow, newCol
+				case Wall:
 					break
+				case Void:
+					if !wrap(&grid, &row, &col, &facing) {
+						break
+					}
+				default:
+					panic("Unexpected tile!")
 				}
-			default:
-				panic("Unexpected tile!")
+			}
+
+			if i < len(instructions) {
+				switch instructions[i] {
+				case 'L':
+					facing = (facing + 3) % 4
+				case 'R':
+					facing = (facing + 1) % 4
+				default:
+					panic("Unexpected instruction!")
+				}
 			}
 		}
 
-		if i < len(instructions) {
-			switch instructions[i] {
-			case 'L':
-				facing = (facing + 3) % 4
-			case 'R':
-				facing = (facing + 1) % 4
-			default:
-				panic("Unexpected instruction!")
-			}
-		}
+		row++
+		col++
+		fmt.Printf("Finished at %d, %d facing %d = %d\n", row, col, facing, 1000 * row + 4 * col + facing)
 	}
-
-	row++
-	col++
-	fmt.Printf("Finished at %d, %d facing %d = %d\n", row, col, facing, 1000 * row + 4 * col + facing)
 }
