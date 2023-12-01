9f4bf191b0face8f68a7502a49dc2c983e87bd99
Schuyler Ullman <schuyler.ullman@gmail.com>
2023-12-01 07:14:25 -0800

    2019: Day 2, part 2

--- 2019/02/day02.go
+++ 2019/02/day02.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
 
 const (
@@ -10,42 +11,58 @@ const (
 	Halt = 99
 )
 
-func runIntcode(positions *[]int) {
-	position := 0
+func runIntcode(program []int) (int, error) {
+	memory := make([]int, len(program))
+	copy(memory, program)
+
+	instructionPointer := 0
 
 	for {
-		switch (*positions)[position] {
+		switch memory[instructionPointer] {
 		case Halt:
-			return
+			instructionPointer += 1
+			return memory[0], nil
 		case Add:
-			(*positions)[(*positions)[position + 3]] = (*positions)[(*positions)[position + 1]] + (*positions)[(*positions)[position + 2]]
+			memory[memory[instructionPointer + 3]] = memory[memory[instructionPointer + 1]] + memory[memory[instructionPointer + 2]]
+			instructionPointer += 4
 		case Multiply:
-			(*positions)[(*positions)[position + 3]] = (*positions)[(*positions)[position + 1]] * (*positions)[(*positions)[position + 2]]
+			memory[memory[instructionPointer + 3]] = memory[memory[instructionPointer + 1]] * memory[memory[instructionPointer + 2]]
+			instructionPointer += 4
 		default:
-			panic("Unexpected operator")
+			return 0, errors.New("Unexpected operator")
 		}
-
-		position += 4
 	}
 }
 
 func main() {
-	positions := make([]int, 0, 10)
+	memory := make([]int, 0, 10)
 
 	for {
 		var value int
 		var sep rune
 		fmt.Scanf("%d%c", &value, &sep)
-		positions = append(positions, value)
+		memory = append(memory, value)
 		if sep != ',' {
 			break
 		}
 	}
 
-	// Restore 1202 state
-	positions[1], positions[2] = 12, 2
+	// Part 1: Run after restoring 1202 state
+	memory[1], memory[2] = 12, 2
 
-	runIntcode(&positions)
+	result, err := runIntcode(memory)
+	fmt.Printf("Part 1: %d\n", result)
 
-	fmt.Println(positions[0])
+	// Part 2: Brute force to find inputs that result in 19690720
+	done:
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			memory[1], memory[2] = noun, verb
+			result, err = runIntcode(memory)
+			if err == nil && result == 19690720 {
+				fmt.Printf("Part 2: %d\n", 100 * noun + verb)
+				break done
+			}
+		}
+	}
 }
