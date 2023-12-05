d191501bd71ea812db005553052c4b50ddad3906
Schuyler Ullman <schuyler.ullman@gmail.com>
2023-12-04 16:06:49 -0800

    Day 4, part 2

--- 2023/04/day04.go
+++ 2023/04/day04.go
@@ -15,10 +15,13 @@ const (
 func main() {
 	var winners [100]bool
 	reader := bufio.NewReader(os.Stdin)
-	sum := 0
+	points := 0
 	section := Preamble
 	num := 0
 	nextValue := 1
+	numWinners := 0
+	cardCounts := make([]int, 0, 100)
+	numPlayed := 0
 
 	for {
 		ch, err := reader.ReadByte()
@@ -29,6 +32,11 @@ func main() {
 		if section == Preamble {
 			if ch == ':' {
 				section = Winners
+				if len(cardCounts) == 0 {
+					cardCounts = append(cardCounts, 0)
+				}
+				cardCounts[0] += 1
+				numPlayed += cardCounts[0]
 			}
 		} else if section == Winners {
 			switch ch {
@@ -46,24 +54,36 @@ func main() {
 			case ' ':
 				if num != 0 && winners[num] {
 					nextValue = nextValue << 1
+					numWinners++
 				}
 				num = 0
 			case '\n':
 				if num != 0 && winners[num] {
 					nextValue = nextValue << 1
+					numWinners++
 				}
-				sum += nextValue >> 1
+				points += nextValue >> 1
+
+				for len(cardCounts) <= numWinners {
+					cardCounts = append(cardCounts, 0)
+				}
+				for i := 1; i <= numWinners; i++ {
+					cardCounts[i] += cardCounts[0]
+				}
 
 				// Reset state for next game
 				section = Preamble
 				winners = [len(winners)]bool{}
+				cardCounts = cardCounts[1:]
 				num = 0
 				nextValue = 1
+				numWinners = 0
 			default:
 				num = num * 10 + int(ch - '0')
 			}
 		}
 	}
 
-	fmt.Printf("%d\n", sum)
+	fmt.Printf("Part 1: %d\n", points)
+	fmt.Printf("Part 2: %d\n", numPlayed)
 }
