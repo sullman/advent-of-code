d8155f9cfc5f379e030a5ccd5f09640f97c03ada
Schuyler Ullman <schuyler.ullman@gmail.com>
2023-12-06 10:09:48 -0800

    Day 6, part 2

--- 2023/06/day06.go
+++ 2023/06/day06.go
@@ -20,6 +20,24 @@ func ExtractNumbers(s string) []int {
 	return nums
 }
 
+func CombineNumbers(a, b int) int {
+	for tmp := b; tmp > 0; tmp /= 10 {
+		a *= 10
+	}
+
+	return a + b
+}
+
+// Solve each one as a math problem using the quadratic formula
+func WaysToWin(time int, distance int) int {
+	// We need to beat it, not tie
+	d := float64(distance + 1)
+	t := float64(time)
+	min := int(math.Ceil((t - math.Sqrt(t * t - 4 * d)) / 2))
+	max := int(math.Floor((t + math.Sqrt(t * t - 4 * d)) / 2))
+	return max - min + 1
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -30,16 +48,15 @@ func main() {
 	distances := ExtractNumbers(scanner.Text())
 
 	total := 1
+	fullTime := 0
+	fullDistance := 0
 
-	// Solve each one as a math problem using the quadratic formula
 	for i := 0; i < len(times); i++ {
-		// We need to beat it, not tie
-		dist := float64(distances[i] + 1)
-		time := float64(times[i])
-		min := int(math.Ceil((time - math.Sqrt(time * time - 4 * dist)) / 2))
-		max := int(math.Floor((time + math.Sqrt(time * time - 4 * dist)) / 2))
-		total *= (max - min + 1)
+		total *= WaysToWin(times[i], distances[i])
+		fullTime = CombineNumbers(fullTime, times[i])
+		fullDistance = CombineNumbers(fullDistance, distances[i])
 	}
 
 	fmt.Printf("Part 1: %d\n", total)
+	fmt.Printf("Part 2: %d\n", WaysToWin(fullTime, fullDistance))
 }
