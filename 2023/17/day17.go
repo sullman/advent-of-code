bdcc57d86b3303b834ff9cbd2e26fcacebec7186
Schuyler Ullman <schuyler.ullman@gmail.com>
2023-12-18 00:45:33 -0800

    Day 17, part 2

--- 2023/17/day17.go
+++ 2023/17/day17.go
@@ -27,7 +27,76 @@ type State struct {
 	dir int
 }
 
-const MaxConsecutive = 3
+func ShortestPath(grid [][]uint8, minConsecutive, maxConsecutive uint8) uint {
+	visited := make([][][]bool, len(grid))
+	for i := 0; i < len(visited); i++ {
+		visited[i] = make([][]bool, len(grid[0]))
+		for j := 0; j < len(grid[0]); j++ {
+			visited[i][j] = make([]bool, len(Movements) << 6)
+		}
+	}
+
+	var state *State
+	candidates := list.New()
+	candidates.PushFront(&State{0, 0, 0, 0, 0})
+	candidates.PushFront(&State{0, 0, 0, 0, 3})
+	// Push a sentinel so we can always InsertBefore
+	candidates.PushBack(&State{0, 0, 0, ^uint(0), 1})
+
+	for {
+		elem := candidates.Front()
+		candidates.Remove(elem)
+		state = elem.Value.(*State)
+
+		if state.row == len(grid) - 1 && state.col == len(grid[0]) - 1 { break }
+
+		for i, delta := range Movements {
+			var newState *State
+
+			if i == state.dir && state.straightRemaining > 0 {
+				newRow := state.row + delta.row
+				newCol := state.col + delta.col
+				if newRow < 0 || newRow >= len(grid) || newCol < 0 || newCol >= len(grid[0]) { continue }
+				newCost := state.cost + uint(grid[newRow][newCol])
+				newState = &State{newRow, newCol, state.straightRemaining - 1, newCost, i}
+			} else if i % 2 != state.dir % 2 {
+				newState = &State{state.row, state.col, maxConsecutive, state.cost, i}
+				newRow := state.row
+				newCol := state.col
+				for j := uint8(0); j < minConsecutive; j++ {
+					newRow += delta.row
+					newCol += delta.col
+					if newRow < 0 || newRow >= len(grid) || newCol < 0 || newCol >= len(grid[0]) {
+						newState = nil
+						break
+					}
+					newState.row, newState.col = newRow, newCol
+					newState.cost += uint(grid[newRow][newCol])
+					newState.straightRemaining -= 1
+				}
+			}
+
+			if newState == nil { continue }
+
+			key := int(newState.straightRemaining << 2) | i
+
+			if visited[newState.row][newState.col][key] {
+				continue
+			} else {
+				visited[newState.row][newState.col][key] = true
+			}
+
+			for e := candidates.Front(); e != nil; e = e.Next() {
+				if newState.cost < e.Value.(*State).cost {
+					candidates.InsertBefore(newState, e)
+					break
+				}
+			}
+		}
+	}
+
+	return state.cost
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -43,61 +112,6 @@ func main() {
 		grid = append(grid, row)
 	}
 
-	visited := make([][][]bool, len(grid))
-	for i := 0; i < len(visited); i++ {
-		visited[i] = make([][]bool, len(grid[0]))
-		for j := 0; j < len(grid[0]); j++ {
-			visited[i][j] = make([]bool, len(Movements) << 2)
-		}
-	}
-
-	var state *State
-	candidates := list.New()
-	candidates.PushFront(&State{0, 0, MaxConsecutive, 0, 1})
-	// Push a sentinel so we can always InsertBefore
-	candidates.PushBack(&State{0, 0, 0, ^uint(0), 1})
-
-	for {
-		elem := candidates.Front()
-		candidates.Remove(elem)
-		state = elem.Value.(*State)
-
-		if state.row == len(grid) - 1 && state.col == len(grid[0]) - 1 { break }
-
-		for i, delta := range Movements {
-			newRow := state.row + delta.row
-			newCol := state.col + delta.col
-			if newRow < 0 || newRow >= len(grid) || newCol < 0 || newCol >= len(grid[0]) { continue }
-
-			newCost := state.cost + uint(grid[newRow][newCol])
-			newState := &State{newRow, newCol, MaxConsecutive - 1, newCost, i}
-
-			if i == state.dir {
-				if state.straightRemaining > 0 {
-					newState.straightRemaining = state.straightRemaining - 1
-				} else {
-					continue
-				}
-			} else if i % 2 == state.dir % 2 {
-				continue
-			}
-
-			key := int(newState.straightRemaining << 2) | i
-
-			if visited[newRow][newCol][key] {
-				continue
-			} else {
-				visited[newRow][newCol][key] = true
-			}
-
-			for e := candidates.Front(); e != nil; e = e.Next() {
-				if newCost < e.Value.(*State).cost {
-					candidates.InsertBefore(newState, e)
-					break
-				}
-			}
-		}
-	}
-
-	fmt.Printf("Part 1: %d\n", state.cost)
+	fmt.Printf("Part 1: %d\n", ShortestPath(grid, 1, 3))
+	fmt.Printf("Part 2: %d\n", ShortestPath(grid, 4, 10))
 }
