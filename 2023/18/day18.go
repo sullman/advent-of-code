7f523b98eece71e0fd2d7e3f9c76e830cdc214a8
Schuyler Ullman <schuyler.ullman@gmail.com>
2023-12-18 10:24:05 -0800

    Day 18, part 2. Now with math!

--- 2023/18/day18.go
+++ 2023/18/day18.go
@@ -4,81 +4,68 @@ import (
 	"fmt"
 )
 
-type Pair struct {
+type Lagoon struct {
 	x int
 	y int
+	area int
+	perimeter int
 }
 
-var Movements = map[rune]Pair{
-	'U': Pair{0, 1},
-	'R': Pair{1, 0},
-	'D': Pair{0, -1},
-	'L': Pair{-1, 0},
+func (l *Lagoon) Dig(dir rune, length int) {
+	l.perimeter += length
+
+	switch dir {
+	case 'R', '0':
+		l.x += length
+		l.area -= l.y * length
+	case 'D', '1':
+		l.y -= length
+		l.area -= l.x * length
+	case 'L', '2':
+		l.x -= length
+		l.area += l.y * length
+	case 'U', '3':
+		l.y += length
+		l.area += l.x * length
+	}
 }
 
-func Encode(x, y int) uint {
-	return uint(((uint32(x) & 0xffff) << 16) | (uint32(y) & 0xffff))
+func (l *Lagoon) Size() int {
+	total := l.perimeter
+	if l.area > 0 {
+		total += l.area
+	} else {
+		total -= l.area
+	}
+
+	return (total >> 1) + 1
 }
 
 func main() {
 	var dir rune
-	var n, length, color int
-	var minX, minY, maxX, maxY int
-	var x, y int
+	var n, length int
 
-	terrain := make(map[uint]int)
+	lagoon1, lagoon2 := new(Lagoon), new(Lagoon)
 
 	for {
-		n, _ = fmt.Scanf("%c %d (#%x)\n", &dir, &length, &color)
-		if n != 3 { break }
+		n, _ = fmt.Scanf("%c %d", &dir, &length)
+		if n != 2 { break }
 
-		move := Movements[dir]
-		for i := 0; i < length; i++ {
-			x += move.x
-			y += move.y
-			if x < minX {
-				minX = x
-			} else if x > maxX {
-				maxX = x
-			}
-			if y < minY {
-				minY = y
-			} else if y > maxY {
-				maxY = y
-			}
-			terrain[Encode(x, y)] = color
-		}
+		lagoon1.Dig(dir, length)
+
+		n, _ = fmt.Scanf("(#%5x%c)\n", &length, &dir)
+		if n != 2 { panic("WTF") }
+
+		lagoon2.Dig(dir, length)
 	}
 
-	if x != 0 || y != 0 {
-		panic("Not a closed loop?")
+	if lagoon1.x != 0 || lagoon1.y != 0 {
+		panic("Part 1: Not a closed loop?")
+	}
+	if lagoon2.x != 0 || lagoon2.y != 0 {
+		panic("Part 2: Not a closed loop?")
 	}
 
-	// Add a moat to ensure we can flood the whole outside
-	minX--
-	minY--
-	maxX++
-	maxY++
-
-	empty := make(map[uint]bool)
-	queue := make([]Pair, 1, len(terrain))
-	queue[0] = Pair{minX, minY}
-	empty[Encode(minX, minY)] = true
-
-	for len(queue) != 0 {
-		pos := queue[0]
-		queue = queue[1:]
-		for _, move := range Movements {
-			x, y = pos.x + move.x, pos.y + move.y
-			if x < minX || x > maxX || y < minY || y > maxY { continue }
-			enc := Encode(x, y)
-			if terrain[enc] == 0 && !empty[enc] {
-				empty[enc] = true
-				queue = append(queue, Pair{x, y})
-			}
-		}
-	}
-
-	totalArea := (maxX - minX + 1) * (maxY - minY + 1)
-	fmt.Printf("Part 1: %d\n", totalArea - len(empty))
+	fmt.Printf("Part 1: %d\n", lagoon1.Size())
+	fmt.Printf("Part 2: %d\n", lagoon2.Size())
 }
