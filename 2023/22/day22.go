7feb13b02ebea84a79747fa4b5b899e238237f69
Schuyler Ullman <schuyler.ullman@gmail.com>
2023-12-22 17:27:14 -0800

    Day 22, part 2

--- 2023/22/day22.go
+++ 2023/22/day22.go
@@ -14,31 +14,27 @@ type Point struct {
 type Block struct {
 	start Point
 	end Point
+	orig [2]Point
 	id int
-	axis int
 	supporting map[int]bool
 	supportedBy map[int]bool
 }
 
 var nextId int
 func NewBlock(p1 Point, p2 Point) *Block {
-	var axis int
 	block := new(Block)
 
 	if p1.x != p2.x {
-		axis = 0
 		if p1.x > p2.x { p1.x, p2.x = p2.x, p1.x }
 	} else if p1.y != p2.y {
-		axis = 1
 		if p1.y > p2.y { p1.y, p2.y = p2.y, p1.y }
 	} else {
-		axis = 2
 		if p1.z > p2.z { p1.z, p2.z = p2.z, p1.z }
 	}
 
 	block.start, block.end = p1, p2
+	block.orig[0], block.orig[1] = p1, p2
 	block.id = nextId
-	block.axis = axis
 	block.supporting = make(map[int]bool)
 	block.supportedBy = make(map[int]bool)
 	nextId++
@@ -59,6 +55,7 @@ func main() {
 
 		if block.end.x > max.x { max.x = block.end.x }
 		if block.end.y > max.y { max.y = block.end.y }
+		if block.end.z > max.z { max.z = block.end.z }
 
 		blocks = append(blocks, block)
 	}
@@ -67,7 +64,7 @@ func main() {
 		return blocks[i].start.z < blocks[j].start.z
 	})
 
-	ground := NewBlock(Point{0, 0, 0}, max)
+	ground := NewBlock(Point{0, 0, 0}, Point{max.x, max.y, 0})
 
 	settled := make([][]*Block, max.x + 1)
 	for x := 0; x < len(settled); x++ {
@@ -102,7 +99,10 @@ func main() {
 	}
 
 	unsafe := make(map[int]bool)
+	byId := make(map[int]*Block)
 	for _, block := range blocks {
+		block.orig[0], block.orig[1] = block.start, block.end
+		byId[block.id] = block
 		if len(block.supportedBy) == 1 {
 			for id, _ := range block.supportedBy {
 				if id == ground.id { continue }
@@ -112,4 +112,75 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", len(blocks) - len(unsafe))
+
+	// Part 2
+	total := 0
+	for _, initial := range blocks {
+		space := make([][][]*Block, max.x + 1)
+
+		for x := 0; x < len(space); x++ {
+			space[x] = make([][]*Block, max.y + 1)
+			for y := 0; y < len(space[0]); y++ {
+				space[x][y] = make([]*Block, max.z + 1)
+				space[x][y][0] = ground
+			}
+		}
+
+		for _, block := range blocks {
+			block.start, block.end = block.orig[0], block.orig[1]
+			if block.id != initial.id {
+				for x := block.start.x; x <= block.end.x; x++ {
+					for y := block.start.y; y <= block.end.y; y++ {
+						for z := block.start.z; z <= block.end.z; z++ {
+							space[x][y][z] = block
+						}
+					}
+				}
+			}
+		}
+
+		moved := make(map[int]bool)
+		queue := make([]*Block, 0)
+		for id, _ := range initial.supporting {
+			queue = append(queue, byId[id])
+		}
+		for len(queue) != 0 {
+			block := queue[0]
+			queue = queue[1:]
+
+			canFall, fell := true, false
+			for canFall {
+				for x := block.start.x; x <= block.end.x; x++ {
+					for y := block.start.y; y <= block.end.y; y++ {
+						if space[x][y][block.start.z - 1] != nil {
+							canFall = false
+						}
+					}
+				}
+
+				if canFall {
+					fell = true
+					for x := block.start.x; x <= block.end.x; x++ {
+						for y := block.start.y; y <= block.end.y; y++ {
+							for z := block.start.z; z <= block.end.z; z++ {
+								space[x][y][z - 1], space[x][y][z] = block, nil
+							}
+						}
+					}
+					block.start.z--
+					block.end.z--
+				}
+			}
+			if fell {
+				moved[block.id] = true
+				for id, _ := range block.supporting {
+					queue = append(queue, byId[id])
+				}
+			}
+		}
+
+		total += len(moved)
+	}
+
+	fmt.Printf("Part 2: %d\n", total)
 }
