9a4eed6448475aac28d19b746d74323efdfd2d7d
Schuyler Ullman <schuyler.ullman@gmail.com>
2024-12-14 09:51:58 -0800

    Day 14, part 2

--- 2024/14/day14.js
+++ 2024/14/day14.js
@@ -6,6 +6,49 @@ const height = 103;
 const midWidth = width >> 1;
 const midHeight = height >> 1;
 
+const DIRECTIONS = [
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+  [0, 1],
+];
+
+function isChristmasTree(robots) {
+  const occupied = new Set();
+  for (const robot of robots) {
+    occupied.add(`${robot.x},${robot.y}`);
+  }
+
+  let withNeighbors = 0;
+  for (const robot of robots) {
+    let hasNeighbor = false;
+    for (const [dx, dy] of DIRECTIONS) {
+      if (occupied.has(`${robot.x + dx},${robot.y + dy}`)) {
+        withNeighbors++;
+        break;
+      }
+    }
+  }
+
+  if (withNeighbors < robots.length / 2) return false;
+
+  // Seems like this is at least a _candidate_, let's look at it
+  const grid = new Array(height);
+  for (let row = 0; row < height; row++) {
+    grid[row] = (new Array(width)).fill(' ');
+  }
+
+  for (const robot of robots) {
+    grid[robot.y][robot.x] = '^';
+  }
+
+  for (const row of grid) {
+    console.log(row.join(''));
+  }
+
+  return true;
+}
+
 function mod(dividend, divisor) {
   let remainder = dividend % divisor;
   if (remainder < 0) remainder += divisor;
@@ -15,10 +58,12 @@ function mod(dividend, divisor) {
 async function run() {
   const rl = readline.createInterface({ input: process.stdin });
   const quadrants = [0, 0, 0, 0];
+  const robots = [];
 
   for await (const line of rl) {
     if (!line) continue;
     const [x, y, dx, dy] = RE.exec(line).slice(1, 5).map(Number);
+    robots.push({ x, y, dx, dy });
     let finalX = mod(x + 100 * dx, width);
     let finalY = mod(y + 100 * dy, height);
 
@@ -38,6 +83,18 @@ async function run() {
   const part1 = quadrants.reduce((prod, num) => prod * num, 1);
 
   console.log('Part 1:', part1);
+
+  let seconds = 0;
+
+  while (!isChristmasTree(robots)) {
+    seconds++;
+    for (const robot of robots) {
+      robot.x = mod(robot.x + robot.dx, width);
+      robot.y = mod(robot.y + robot.dy, height);
+    }
+  }
+
+  console.log('Part 2:', seconds);
 }
 
 run().then(() => {
