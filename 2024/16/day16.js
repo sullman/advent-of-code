5aeadd20606c1d3590b770ab7b6ee025c4e0bb64
Schuyler Ullman <schuyler.ullman@gmail.com>
2024-12-15 22:11:18 -0800

    Day 16, part 2

--- 2024/16/day16.js
+++ 2024/16/day16.js
@@ -8,11 +8,21 @@ const DIRECTIONS = [
   [1, 0],
 ];
 
+function addAll(set, values) {
+  for (const val of values) {
+    set.add(val);
+  }
+}
+
 function insertSorted(queue, seen, state) {
   const key = `${state.row},${state.col},${state.dir}`;
   const prev = seen.get(key);
-  if (prev && prev <= state.score) return;
-  seen.set(key, state.score);
+  if (prev?.score === state.score) {
+    addAll(prev.path, state.path);
+    return;
+  }
+  if (prev && prev.score < state.score) return;
+  seen.set(key, state);
 
   for (let i = 0; i < queue.length; i++) {
     if (state.score < queue[i].score) {
@@ -47,28 +57,42 @@ async function run() {
 
   const queue = [];
   const seen = new Map();
+  let best;
+  const allUsedTiles = new Set();
 
   queue.push({
     row,
     col,
     dir: 0,
     score: 0,
+    path: new Set([`${row},${col}`]),
     // bestPossible: Math.abs(endRow - row) + Math.abs(endCol - col) + TURN_SCORE
   });
 
   while (queue.length) {
     const state = queue.shift();
+    if (best && state.score > best) break;
+
+    if (grid[state.row][state.col] === 'E') {
+      if (!best) {
+        console.log('Part 1:', state.score);
+        best = state.score;
+      }
+
+      addAll(allUsedTiles, state.path);
+
+      continue;
+    }
+
     const dir = DIRECTIONS[state.dir];
-    if (grid[state.row + dir[0]][state.col + dir[1]] === '.') {
+    if (grid[state.row + dir[0]][state.col + dir[1]] !== '#') {
       insertSorted(queue, seen, {
         row: state.row + dir[0],
         col: state.col + dir[1],
         dir: state.dir,
-        score: state.score + 1
+        score: state.score + 1,
+        path: new Set([...state.path, `${state.row + dir[0]},${state.col + dir[1]}`])
       });
-    } else if (grid[state.row + dir[0]][state.col + dir[1]] === 'E') {
-      console.log('Part 1:', state.score + 1);
-      break;
     }
 
     insertSorted(queue, seen, {
@@ -82,6 +106,8 @@ async function run() {
       score: state.score + TURN_SCORE
     });
   }
+
+  console.log('Part 2:', allUsedTiles.size);
 }
 
 run().then(() => {
