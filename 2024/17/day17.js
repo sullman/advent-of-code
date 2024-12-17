5e48eded7e3383b3c4383f7c2dd071919f069757
Schuyler Ullman <schuyler.ullman@gmail.com>
2024-12-17 00:10:11 -0800

    Day 14, part 2

--- 2024/17/day17.js
+++ 2024/17/day17.js
@@ -1,21 +1,8 @@
 const readline = require('node:readline');
 
-async function run() {
-  const rl = readline.createInterface({ input: process.stdin });
-  let registers = {};
-  let program = [];
+function execute(program, registers, onlyOnce) {
   let instruction = 0;
   let output = [];
-
-  for await (const line of rl) {
-    if (!line) continue;
-    const match = /Register ([ABC]): (\d+)/.exec(line);
-    if (match) {
-      registers[match[1]] = parseInt(match[2], 10);
-    } else {
-      program = line.slice('Program: '.length).split(',').map(Number);
-    }
-  }
 
   const combo = operand => {
     switch (operand) {
@@ -62,6 +49,7 @@ async function run() {
       break;
     case 5:
       output.push(0b111 & combo(operand));
+      if (onlyOnce) return output;
       break;
     case 6:
       registers.B = registers.A >> combo(operand);
@@ -74,7 +62,42 @@ async function run() {
     }
   }
 
-  console.log('Part 1:', output.join(','));
+  return output;
+}
+
+function solveBackwards(program, a, index) {
+  if (index < 0) return a;
+
+  for (let b = 0; b < 8; b++) {
+    // Can't use (a << 3) | b because JS bitwise operators only work on 32 bits!
+    const [out] = execute(program, { A: (a * 8) + b, B: 0, C: 0}, true);
+    if (out === program[index]) {
+      const solution = solveBackwards(program, (a * 8) + b, index - 1);
+      if (solution) return solution;
+    }
+  }
+
+  return;
+}
+
+async function run() {
+  const rl = readline.createInterface({ input: process.stdin });
+  let registers = {};
+  let program = [];
+
+  for await (const line of rl) {
+    if (!line) continue;
+    const match = /Register ([ABC]): (\d+)/.exec(line);
+    if (match) {
+      registers[match[1]] = parseInt(match[2], 10);
+    } else {
+      program = line.slice('Program: '.length).split(',').map(Number);
+    }
+  }
+
+  console.log('Part 1:', execute(program, registers).join(','));
+
+  console.log('Part 2:', solveBackwards(program, 0, program.length - 1));
 }
 
 run().then(() => {
