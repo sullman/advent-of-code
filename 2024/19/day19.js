126befdcc1beed0a34df04eb4c1f3a144adead1c
Schuyler Ullman <schuyler.ullman@gmail.com>
2024-12-19 08:36:31 -0800

    Day 19, part 2

--- 2024/19/day19.js
+++ 2024/19/day19.js
@@ -1,38 +1,63 @@
 const readline = require('node:readline');
 
-function optimizeRegex(designs) {
-  designs.sort((a, b) => a.length - b.length);
-  const necessary = [];
-  let len = designs[0].length;
-  let re = /^$/;
-
-  for (const design of designs) {
-    if (design.length > len) {
-      len = design.length;
-      re = new RegExp(`^(${necessary.join('|')})+$`);
+function addSegment(tree, segment) {
+  let node = tree;
+  for (const ch of segment) {
+    if (!node.children.has(ch)) {
+      node.children.set(ch, { children: new Map() });
     }
 
-    if (!re.test(design)) necessary.push(design);
+    node = node.children.get(ch);
   }
 
-  return new RegExp(`^(${necessary.join('|')})+$`);
+  node.valid = true;
+}
+
+function countSolutions(tree, cache, str) {
+  if (!str) return 1;
+
+  if (cache.has(str)) return cache.get(str);
+
+  let solutions = 0;
+  let node = tree;
+
+  for (let i = 0; i < str.length; i++) {
+    node = node.children.get(str[i]);
+    if (!node) break;
+    if (node.valid) {
+      solutions += countSolutions(tree, cache, str.slice(i + 1));
+    }
+  }
+
+  cache.set(str, solutions);
+
+  return solutions;
 }
 
 async function run() {
   const rl = readline.createInterface({ input: process.stdin });
-  let re;
+  const tree = { children: new Map() };
+  const cache = new Map();
   let part1 = 0;
+  let part2 = 0;
 
   for await (const line of rl) {
     if (!line) continue;
-    if (!re) {
-      re = optimizeRegex(line.split(', '));
+    if (tree.children.size === 0) {
+      for (const segment of line.split(', ')) {
+        addSegment(tree, segment);
+      }
     } else {
-      part1 += re.test(line);
+      const num = countSolutions(tree, cache, line);
+      if (num) {
+        part1++;
+        part2 += num;
+      }
     }
   }
 
   console.log('Part 1:', part1);
+  console.log('Part 2:', part2);
 }
 
 run().then(() => {
