89637d3b74c67849fd0e1d00ffd846ad782de9f1
Schuyler Ullman <schuyler.ullman@gmail.com>
2024-12-19 23:04:53 -0800

    Day 20, part 2

--- 2024/20/day20.js
+++ 2024/20/day20.js
@@ -7,20 +7,12 @@ const DIRECTIONS = [
   [0, 1],
 ];
 
-const cache = new Map();
-function honestDistance(grid, row, col) {
-  const key = `${row},${col}`;
-  if (cache.has(key)) return cache.get(key);
-
-  let distance = 0;
-
-  cache.set(key, distance);
-  return distance;
-}
-
 function computeDistances(grid, endRow, endCol) {
-  const distances = new Map();
-  distances.set(`${endRow},${endCol}`, 0);
+  const distances = new Array(grid.length);
+  for (let i = 0; i < distances.length; i++) {
+    distances[i] = new Array(grid[i].length);
+  }
+  distances[endRow][endCol] = 0;
   const queue = [[endRow, endCol, 0]];
 
   while (queue.length) {
@@ -29,14 +21,56 @@ function computeDistances(grid, endRow, endCol) {
       const r = row + dir[0];
       const c = col + dir[1];
       const val = grid[r]?.[c];
-      if (!distances.has(`${r},${c}`) && val && val !== '#') {
-        distances.set(`${r},${c}`, distance + 1);
+      if (distances[r][c] === undefined && val && val !== '#') {
+        distances[r][c] = distance + 1;
         queue.push([r, c, distance + 1]);
       }
     }
   }
 
   return distances;
+}
+
+function findShortcuts(grid, distances, startRow, startCol, numCheats) {
+  const honest = distances[startRow][startCol];
+  const threshold = 100;
+  let row = startRow;
+  let col = startCol;
+  let numShortcuts = 0;
+
+  const isShortcut = (r, c, dr, dc) => {
+    const newRow = r + dr;
+    const newCol = c + dc;
+    const dist = Math.abs(dr) + Math.abs(dc) + distances[newRow]?.[newCol];
+    return dist <= distances[r][c] - threshold;
+  }
+
+  while (distances[row][col] > threshold) {
+    for (let dr = 0; dr <= numCheats; dr++) {
+      for (let dc = 0; dc <= numCheats - dr; dc++) {
+        numShortcuts += isShortcut(row, col, dr, dc);
+        if (dc) {
+          numShortcuts += isShortcut(row, col, dr, -dc);
+        }
+        if (dr) {
+          numShortcuts += isShortcut(row, col, -dr, dc);
+          if (dc) {
+            numShortcuts += isShortcut(row, col, -dr, -dc);
+          }
+        }
+      }
+    }
+
+    for (const dir of DIRECTIONS) {
+      if (distances[row][col] === distances[row + dir[0]]?.[col + dir[1]] + 1) {
+        row = row + dir[0];
+        col = col + dir[1];
+        break;
+      }
+    }
+  }
+
+  return numShortcuts;
 }
 
 async function run() {
@@ -62,66 +96,8 @@ async function run() {
 
   const distances = computeDistances(grid, endRow, endCol);
 
-  const queue = [{
-    row: startRow,
-    col: startCol,
-    distance: 0,
-    cheatsRemaining: 1,
-    visited: new Set()
-  }];
-  const solutions = [];
-  const honest = distances.get(`${startRow},${startCol}`);
-
-  while (queue.length) {
-    const state = queue.shift();
-
-    if (!state.cheatsRemaining) {
-      solutions.push(state.distance + distances.get(`${state.row},${state.col}`));
-      continue;
-    }
-    if (state.distance > honest - 100) break;
-    if (grid[state.row][state.col] === 'E') {
-      solutions.push(state.distance);
-      break;
-    }
-
-    state.visited.add(`${state.row},${state.col}`);
-
-    for (const dir of DIRECTIONS) {
-      let row = state.row + dir[0];
-      let col = state.col + dir[1];
-      if (state.visited.has(`${row},${col}`)) continue;
-      if (grid[row]?.[col] === '#' && state.cheatsRemaining) {
-        row += dir[0];
-        col += dir[1];
-        if (grid[row]?.[col] && grid[row][col] !== '#') {
-          queue.push({
-            row,
-            col,
-            distance: state.distance + 2,
-            cheatsRemaining: state.cheatsRemaining - 1,
-            visited: new Set(state.visited)
-          });
-        }
-      } else if (grid[row]?.[col]) {
-        queue.push({
-          row,
-          col,
-          distance: state.distance + 1,
-          cheatsRemaining: state.cheatsRemaining,
-          visited: new Set(state.visited)
-        });
-      }
-    }
-  }
-
-  let part1 = 0;
-
-  for (const speed of solutions) {
-    if (honest - speed >= 100) part1++;
-  }
-
-  console.log('Part 1:', part1);
+  console.log('Part 1:', findShortcuts(grid, distances, startRow, startCol, 2));
+  console.log('Part 2:', findShortcuts(grid, distances, startRow, startCol, 20));
 }
 
 run().then(() => {
