ba42a18c02e2e077365df791df8cbbf27a2c8eb2
Schuyler Ullman <schuyler.ullman@gmail.com>
2024-12-21 23:03:09 -0800

    Day 21, part 2

--- 2024/21/day21.js
+++ 2024/21/day21.js
@@ -22,39 +22,74 @@ const DIRPAD = {
   '>': [1, 2],
 };
 
-function* generateMovements(pad, sequence) {
-  let [row, col] = pad['A'];
+const controlCache = new Map();
+function generateControls(pad, over, next) {
+  const key = `${over}${next}`;
+  if (controlCache.has(key)) return controlCache.get(key);
 
-  for (const ch of sequence) {
-    const [nextRow, nextCol] = pad[ch];
-    const canGoLeft = row !== pad['A'][0] || nextCol !== 0;
-    const canGoDown = col !== 0 || nextRow !== pad['A'][0];
-    for (; canGoLeft && col > nextCol; col--) yield '<';
-    for (; canGoDown && row < nextRow; row++) yield 'v';
-    for (; row > nextRow; row--) yield '^';
-    for (; col < nextCol; col++) yield '>';
-    for (; !canGoLeft && col > nextCol; col--) yield '<';
-    for (; !canGoDown && row < nextRow; row++) yield 'v';
-    yield 'A';
+  const controls = [];
+  const [nextRow, nextCol] = pad[next];
+  let [row, col] = pad[over];
+  const canGoLeft = row !== pad['A'][0] || nextCol !== 0;
+  const canGoDown = col !== 0 || nextRow !== pad['A'][0];
+  for (; canGoLeft && col > nextCol; col--) controls.push('<');
+  for (; canGoDown && row < nextRow; row++) controls.push('v');
+  for (; canGoDown && row > nextRow; row--) controls.push('^');
+  for (; col < nextCol; col++) controls.push('>');
+  for (; !canGoDown && row > nextRow; row--) controls.push('^');
+  for (; !canGoLeft && col > nextCol; col--) controls.push('<');
+  for (; !canGoDown && row < nextRow; row++) controls.push('v');
+  controls.push('A');
+
+  controlCache.set(key, controls);
+  return controls;
+}
+
+const lengthCache = new Map();
+function countRequiredMoves(over, next, depth) {
+  const key = `${over}${next}:${depth}`;
+  if (lengthCache.has(key)) return lengthCache.get(key);
+
+  let len = 0;
+  const controls = ['A', ...generateControls(DIRPAD, over, next)];
+
+  if (depth === 1) {
+    len = controls.length - 1;
+  } else {
+    for (let i = 1; i < controls.length; i++) {
+      len += countRequiredMoves(controls[i - 1], controls[i], depth - 1);
+    }
   }
+
+  lengthCache.set(key, len);
+  return len;
 }
 
 async function run() {
   const rl = readline.createInterface({ input: process.stdin });
   let part1 = 0;
+  let part2 = 0;
 
   for await (const line of rl) {
     if (!line) continue;
-    let iter = generateMovements(KEYPAD, line.split(''));
-    for (let i = 0; i < 2; i++) {
-      iter = generateMovements(DIRPAD, iter);
+    let len1 = 0;
+    let len2 = 0;
+    const sequence = ['A', ...line.split('')];
+
+    for (let i = 1; i < sequence.length; i++) {
+      const controls = ['A', ...generateControls(KEYPAD, sequence[i - 1], sequence[i])];
+      for (let j = 1; j < controls.length; j++) {
+        len1 += countRequiredMoves(controls[j - 1], controls[j], 2);
+        len2 += countRequiredMoves(controls[j - 1], controls[j], 25);
+      }
     }
-    let len = 0;
-    for (const ch of iter) len++;
-    part1 += parseInt(line, 10) * len;
+
+    part1 += parseInt(line, 10) * len1;
+    part2 += parseInt(line, 10) * len2;
   }
 
   console.log('Part 1:', part1);
+  console.log('Part 2:', part2);
 }
 
 run().then(() => {
