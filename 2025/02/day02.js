9af2923e233a1a9a200e9032af02650674105147
Schuyler Ullman <schuyler.ullman@gmail.com>
2025-12-09 23:02:11 -0800

    Day 2, part 2

--- 2025/02/day02.js
+++ 2025/02/day02.js
@@ -2,45 +2,53 @@ const readline = require('node:readline');
 
 const PATTERN = /(\d+)-(\d+)/g;
 
-function sumInvalidInRange(low, high) {
-  let sum = 0;
+function findInvalidInRange(low, high, numRepeats) {
+  const invalid = new Set();
   let begin;
   let end;
 
-  if (low.toString().length % 2) {
-    begin = 10 ** (low.toString().length >> 1);
+  if (low.toString().length % numRepeats) {
+    begin = 10 ** (Math.floor(low.toString().length / numRepeats));
   } else {
-    begin = Math.floor(low / (10 ** (low.toString().length >> 1)));
+    begin = Number(low.toString().slice(0, Math.floor(low.toString().length / numRepeats)));
   }
 
-  if (high.toString().length % 2) {
-    end = 10 ** (high.toString().length >> 1) - 1;
+  if (high.toString().length % numRepeats) {
+    end = 10 ** (Math.floor(high.toString().length / numRepeats)) - 1;
   } else {
-    end = Math.floor(high / (10 ** (high.toString().length >> 1)))
+    end = Number(high.toString().slice(0, Math.floor(high.toString().length / numRepeats)));
   }
 
   for (let i = begin; i <= end; i++) {
-    const candidate = Number(i.toString().repeat(2));
+    const candidate = Number(i.toString().repeat(numRepeats));
     if (candidate < low || candidate > high) continue;
-    sum += candidate;
+    invalid.add(candidate);
   }
 
-  return sum;
+  return invalid;
 }
 
 async function run() {
   const rl = readline.createInterface({ input: process.stdin });
 
-  let part1 = 0;
+  let part1 = new Set();
+  let part2 = new Set();
 
   for await (const line of rl) {
     for (const match of line.matchAll(PATTERN)) {
-      const [_, low, high] = match;
-      part1 += sumInvalidInRange(parseInt(low, 10), parseInt(high, 10));
+      const [_, low, high] = match.map(Number);
+      part1 = part1.union(findInvalidInRange(low, high, 2));
+
+      for (let i = 3; i <= high.toString().length; i++) {
+        part2 = part2.union(findInvalidInRange(low, high, i));
+      }
     }
   }
 
-  console.log('Part 1:', part1);
+  part2 = part2.union(part1);
+
+  console.log('Part 1:', [...part1].reduce((a, b) => a + b));
+  console.log('Part 2:', [...part2].reduce((a, b) => a + b));
 }
 
 run().then(() => {
