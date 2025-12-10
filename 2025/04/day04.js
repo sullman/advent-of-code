2735415545b2464d85b3fb55157c890743be53fa
Schuyler Ullman <schuyler.ullman@gmail.com>
2025-12-10 09:52:34 -0800

    Day 4, part 2

--- 2025/04/day04.js
+++ 2025/04/day04.js
@@ -11,6 +11,25 @@ const NEIGHBORS = [
   [1, 1],
 ];
 
+function removeRoll(rolls, neighbors, row, col) {
+  if (!rolls.has(`${row},${col}`)) return 0;
+
+  let numRemoved = 1;
+
+  rolls.delete(`${row},${col}`);
+
+  for (const [dr, dc] of NEIGHBORS) {
+    const key = `${row + dr},${col + dc}`;
+    const previousCount = neighbors.get(key);
+    neighbors.set(key, previousCount - 1);
+    if (previousCount === 4) {
+      numRemoved += removeRoll(rolls, neighbors, row + dr, col + dc);
+    }
+  }
+
+  return numRemoved;
+}
+
 async function run() {
   const rl = readline.createInterface({ input: process.stdin });
 
@@ -33,11 +52,18 @@ async function run() {
     row++;
   }
 
+  let part2 = 0;
+
   for (const key of rolls) {
-    if ((neighbors.get(key) ?? 0) < 4) part1++;
+    if ((neighbors.get(key) ?? 0) < 4) {
+      part1++;
+      const [r, c] = key.split(',').map(Number);
+      part2 += removeRoll(rolls, neighbors, r, c);
+    }
   }
 
   console.log('Part 1:', part1);
+  console.log('Part 2:', part2);
 }
 
 run().then(() => {
