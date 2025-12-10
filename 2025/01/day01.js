7c47b8a20a57a113e48b870c2db27717fc178a92
Schuyler Ullman <schuyler.ullman@gmail.com>
2025-12-09 16:22:01 -0800

    Day 1, part 2

--- 2025/01/day01.js
+++ 2025/01/day01.js
@@ -7,19 +7,31 @@ async function run() {
   const rl = readline.createInterface({ input: process.stdin });
 
   let position = 50;
-  let zeroes = 0;
+  let part1 = 0;
+  let part2 = 0;
 
   for await (const line of rl) {
     const match = PATTERN.exec(line);
     if (!match) break;
 
-    const delta = (match[1] === 'L' ? -1 : 1) * parseInt(match[2], 10);
-    position = (position + delta) % SIZE;
-    // if (position < 0) position += SIZE;
-    if (position === 0) zeroes++;
+    if (position === 0 && match[1] === 'L') part2--;
+    position += (match[1] === 'L' ? -1 : 1) * parseInt(match[2], 10);
+    if (position === 0) part2++;
+
+    while (position >= SIZE) {
+      position -= SIZE;
+      part2++;
+    }
+    while (position < 0) {
+      position += SIZE;
+      part2++;
+      if (position === 0) part2++;
+    }
+    if (position === 0) part1++;
   }
 
-  console.log('Part 1:', zeroes);
+  console.log('Part 1:', part1);
+  console.log('Part 2:', part2);
 }
 
 run().then(() => {
